f3133e35b41802fd1ad005d5854a62b42b6830ec
FlMondon <florian.mondon@clermont.in2p3.fr>
2018-11-13 10:32:29 +0100

    update data_table

--- sugar_analysis/data_table.py
+++ sugar_analysis/data_table.py
@@ -11,7 +11,7 @@ import numpy as np
 import sncosmo
 from sugar_analysis import constant as cst
 from astropy.table import Table
-from scipy.interpolate import InterpolatedUnivariateSpline as Spline1d
+#from scipy.interpolate import InterpolatedUnivariateSpline as Spline1d
 from sugar_analysis import builtins
 import cPickle as pkl
 output_path = '../../'
@@ -33,22 +33,92 @@ class build_data(object):
         builtins.register_SNf_bands_width(width=10)
         builtins.mag_sys_SNF_width(width=10)
         builtins.register_SUGAR()  
+        self.noTH = True
         self.spec = pkl.load(open(output_path+
-                                  'sugar_analysis_data/spectra_table/meta_spectra.pkl'))
+                                  'sugar_analysis_data/SNF-0203-CABALLO2/meta_spectra.pkl'))
         self.meta= pkl.load(open(meta))
-        self.bsd = extract_spectra()
+      
+    def integral_to_phot(self, wave, flux, var=None):
+
+        self.start = min(wave)
+        self.end =  max(wave)
+        # Interpolate filter over spectrum (natively regularly sampled)
+        f = self.interpolate(wave)
+        f *= wave/(cst.CLIGHT*1.0e13*cst.HPLANCK)
         
-    def integral_to_phot(self, wave, flux, band):
-        filt2 = np.genfromtxt(output_path+
-                              'sugar_analysis_data/data/Instruments/Florian/'+band+'.dat')
-        wlen = filt2[:,0]
-        tran = filt2[:,1]
-        self.splB = Spline1d(wlen, tran, k=1,ext = 1)    
         #computation of the integral
-        dxs = (float(max(wave)-min(wave))/(len(wave)-1))
-        inte = np.sum(flux*wave/(cst.CLIGHT*1.0e13*cst.HPLANCK)*self.splB(wave)*dxs)
-        return inte
-    def build_Astropy_Table(self, sn_name, band_used=['new_fI_10','fB_10','fV_10','fR_10','fU_10']):
+        dxs = (float(self.end - self.start)/(len(wave)-1))
+        phot = np.sum(flux*f*dxs)
+        if  isinstance(var, np.ndarray):
+            dphot = np.sqrt((var*f**2).sum())*dxs
+        else:
+            dphot = None
+#        for i in range(len(f)):
+#            if f[i] > 1:
+#                print '%e'%(f[i])
+#        print '############################################'
+        return phot, dphot
+    
+    def interpolate(self, x):
+        """
+        Interpolate filter over input array x (not necessarily
+        regularly sampled).
+
+        Beware that interpolation doesn't go wild *outside* the filter
+        definition range, or even *inside*.
+        """
+
+        from scipy.interpolate import UnivariateSpline, pchip
+        filt2 = np.genfromtxt(output_path+
+                              'sugar_analysis_data/data/Instruments/Florian/'+self.band+'.dat')
+        transp = 0.000
+        for i, trans in enumerate(filt2[:,1]):
+            if trans > 0.000 and transp == 0.000:
+                start = filt2[:,0][i]
+            if transp > 0.000 and trans == 0.000:
+                end = filt2[:,0][i-1]
+            transp = trans
+        y = self.pixel_fracs(x, start, end)
+        
+        if self.noTH :
+            inside = y > 0
+            spline = UnivariateSpline(filt2[:,0], filt2[:,1], s=0)
+            y[inside] = np.maximum(spline(x[inside]), 0)
+        return y
+
+    def pixel_fracs(self, x, xmin, xmax):
+        """Return which fraction of each (Voronoi) pixel centered on (not
+        necessaryly regularly sampled) x is contained within wavelength
+        range [xmin,xmax]."""
+    
+        x = np.asarray(x)        # Center of pixels
+        n = len(x)
+    
+        # Pixel boundaries
+        dx = np.diff(x) / 2        # Half px width
+        xb = np.concatenate(([x[0] - dx[0]],    # 1st px left boundary
+                            x[1:] - dx,        # Middle pixel left boundaries
+                            [x[-1] + dx[-1]]))  # Last px right boundary
+    
+        # Fractions
+        f = np.zeros(n)
+        f[(xmin <= x) & (x <= xmax)] = 1.  # Rough approach 1st
+    
+        # Compute precise fractions at domain boundaries
+        imin = xb.searchsorted(xmin)  # xb[imin-1] < xmin < xb[imin]
+        imax = xb.searchsorted(xmax)  # xb[imax-1] < xmax < xb[imax]
+        if 0 < imin <= n:
+            f[imin - 1] = (min(xb[imin], xmax) - xmin) / (xb[imin] - xb[imin - 1])
+        if 0 < imax <= n:
+            f[imax - 1] = (xmax - max(xb[imax - 1], xmin)) / \
+                (xb[imax] - xb[imax - 1])
+#        if self.band == 'RSNf':
+#            for i in range(len(f)):
+#                if f[i]>0.1:
+#                    print x[i], f[i]       
+        return f
+
+    def build_Astropy_Table(self, sn_name, band_used=['new_fU_10','fB_10','fV_10','fR_10','new_fI_10']):
         """
         """    
         time = []
@@ -59,47 +129,23 @@ class build_data(object):
         zpsys = []
         vega = sncosmo.get_magsystem('vega_snf_10')
 
-        for nspec in self.bsd.dico_spectra[sn_name]:
-            p = self.bsd.dico_spectra[sn_name][nspec]['days']
-            for b in band_used:
-                phot_flux = self.integral_to_phot(
-                        self.bsd.dico_spectra[sn_name][nspec]['X'],self.bsd.dico_spectra[sn_name][nspec]['Y'],b)  
-
+        
+        for b in band_used:
+            self.band = b
+            if b == 'fB_10' or b == 'new_fU_10':
+                spectra = 'spectra_B'
+            elif b == 'fV_10' or  b == 'fR_10' or  b == 'new_fI_10':
+                spectra = 'spectra_R'
+            for p in self.spec[sn_name][spectra].keys():
+                phot_flux, dphot = self.integral_to_phot(
+                        self.spec[sn_name][spectra][str(p)]['wave'],
+                        self.spec[sn_name][spectra][str(p)]['flux'],
+                        var=self.spec[sn_name][spectra][str(p)]['var']) 
                 flux.append(phot_flux)
-                time.append(p)
-                band.append(b)
-                flux_err.append(None)
-                zp.append(2.5*np.log10(vega.zpbandflux(b)))
-                zpsys.append('vega_snf_10')
-                
-        data = Table([time, band, flux, flux_err, zp, zpsys], 
-                     names=('time', 'band', 'flux', 'fluxerr', 'zp', 'zpsys'), 
-                     meta={'name': 'data'})
-        return data
-
-
-    def build_Astropy_Table_flo(self, sn_name, band_used=['new_fI_10','fB_10','fV_10','fR_10','fU_10']):
-        """
-        """    
-        time = []
-        flux = []
-        flux_err = []
-        band = []
-        zp = []
-        zpsys = []
-        vega = sncosmo.get_magsystem('vega_snf_10')
-
-        for p in self.spec[sn_name]['spectra'].keys():
-            for b in band_used:
-                phot_flux = self.integral_to_phot(
-                        self.spec[sn_name]['spectra'][str(p)]['wave'],
-                        self.spec[sn_name]['spectra'][str(p)]['flux'],b)  
-
-                flux.append(phot_flux)
-                time.append(p)
-                band.append(b)
-                flux_err.append(None)
-                zp.append(2.5*np.log10(vega.zpbandflux(b)))
+                time.append(float(p))
+                band.append(self.band)
+                flux_err.append(dphot*self.meta[sn_name]['target.errorscale'])
+                zp.append(2.5*np.log10(vega.zpbandflux(self.band)))
                 zpsys.append('vega_snf_10')
                 
         data = Table([time, band, flux, flux_err, zp, zpsys], 
--- sugar_analysis/fitting_lc.py
+++ sugar_analysis/fitting_lc.py
@@ -7,50 +7,76 @@ Created on Wed Sep  5 10:33:31 2018
 """
 
 import sncosmo
-import builtins_SNF as Build_SNF
+import builtins
 import numpy as np
+import cosmo_tools as ct
 import copy
 import constant as cst
-import read_SNf_data as RSNf
+from data_table import build_data
 import cPickle as pkl
-
+import os
 SNf_path = '../../sugar_analysis_data/META-CABALLO2.pkl'
 output_path = '../../'
 
 class LC_Fitter(object):
     
-    def __init__(self, model_name='sugar', sample='SNf', data=None , t0_fix=False, sub_sample=None, modelcov=False):
-        
+    def __init__(self, model_name='sugar', sample='SNf', data=None, 
+                 t0_fix=False, sub_sample=None, modelcov=False, 
+                 filters=['new_fU_10','fB_10','fV_10','fR_10','new_fI_10'], 
+                 width=10):
+        self.fitting_sample =  False
         self.model_name = model_name
         self.t0_fix = t0_fix
         self.dic_res = None
         self.sub_sample = sub_sample
         self.sample = sample
         self.modelcov = modelcov
-        Build_SNF.register_SUGAR()
+        self.bd = build_data()#
+        self.filters = filters
+        builtins.register_SUGAR()
         
         if self.sample=='SNf':
             self.meta = pkl.load(open(SNf_path))
-            self.data = {}
+            self.data = []
             if sub_sample==None:
                 for sn_name in self.meta.keys():
                     if self.meta[sn_name]['idr.subset'] == 'training' or self.meta[sn_name]['idr.subset'] == 'validation':
-                        self.data[sn_name] = self.meta[sn_name]
+                        self.data.append(sn_name)
             else :
                 for sn_name in self.sub_sample:
-                    self.data[sn_name] = self.meta[sn_name]
+                    self.data.append(sn_name)
                     
-            self.filters = ['BSNf','VSNf','RSNf']
+            
             self.errorscale = True
-            self.width= 10
-            Build_SNF.register_SNf_bands_width(width=self.width)
-            Build_SNF.mag_sys_SNF_width(width=self.width)
+            self.width= width
+            builtins.register_SNf_bands_width(width=self.width)
+            builtins.mag_sys_SNF_width(width=self.width)
             
         elif sample=='jla':
-            raise ValueError('jla not implemented yet')
+            builtins.builtins_jla_bandpasses()
+            builtins.mag_sys_jla()
+            datos = os.listdir(output_path+'sugar_analysis_data/data/jla_data/jla_light_curves/')
+            self.data = []
+            for sn_name in datos:
+                if sub_sample==None:
+                    if sn_name.startswith('lc-'):
+                        self.data.append(sn_name)
+            else :
+                if sub_sample=='SDSS':
+                    if sn_name.startswith('lc-SDSS'):
+                        self.data.append(sn_name)      
+                elif sub_sample=='SNLS':
+                    raise ValueError ('SNLS not implemented yet')
+                elif sub_sample=='Nearby':
+                    raise ValueError ('Nearby not implemented yet') 
+                elif sub_sample=='HST':
+                    raise ValueError ('HST not implemented yet')                
+                else:
+                    for sn_name in self.sub_sample:
+                        self.data.append(sn_name)         
         else:
-            Build_SNF.register_SNf_bands_width(width=self.width)
-            Build_SNF.mag_sys_SNF_width(width=self.width)
+            builtins.register_SNf_bands_width(width=self.width)
+            builtins.mag_sys_SNF_width(width=self.width)
             print 'Warning : Mag sys and band used in your data have to be register in sncosmo our in builtins_SNF'
             try:
                 self.data = pkl.load(open(data))
@@ -62,11 +88,9 @@ class LC_Fitter(object):
     def sn_data(self, sn_name):
        
         if self.sample == 'SNf':
-            self.table_sn, self.zhl, self.zerr, self.zcmb, self.mwebv, self.daymax = RSNf.read_meta_SNF(self.data, sn_name,
-                                               filters=self.filters, 
-                                               model=self.model_name, 
-                                               errorscale=self.errorscale,
-                                               width=self.width)
+            self.zhl, self.zerr, self.zcmb, self.mwebv, self.daymax = self.meta[sn_name]['host.zhelio'], self.meta[sn_name]['host.zcmb'], self.meta[sn_name]['host.zhelio.err'], self.meta[sn_name]['target.mwebv'], self.meta[sn_name]['salt2.DayMax']
+            self.table_sn = self.bd.build_Astropy_Table(sn_name, band_used=self.filters)
+        
         elif self.sample == 'jla':
             raise ValueError('jla not implemented yet')
             
@@ -83,7 +107,7 @@ class LC_Fitter(object):
                     raise ValueError('t0_fix is True: need daymax in the dictionary data')
             
     
-    def fit_lc_sugar(self, data, zhl=None,  mwebv=None):
+    def fit_lc_sugar(self, data, zhl=None, zcmb=None, mwebv=None):
         """
         """
         if not self.fitting_sample :
@@ -95,23 +119,34 @@ class LC_Fitter(object):
                               effect_frames=['obs']) 
             self.mwebv = mwebv
             self.zhl = zhl
-            
-        self.model.set(mwebv=self.mwebv)
-        self.model.set(z=self.zhl)
-        
+            #If you don't have zcmb we used only zhl to compute the distance
+            #modulus for the guess of Xgr
+            if zcmb == None:
+                self.zcmb = zhl
+            else:
+                self.zcmb = zcmb
+            if mwebv == None:
+                self.model.set(mwebv=0.)
+            else: 
+                self.model.set(mwebv=self.mwebv)
+            if zhl==None:
+                self.model.set(z=0.)
+                Xgr_init = 1.
+            else:
+                self.model.set(z=self.zhl)
+                Xgr_init = 10**(-0.4*(ct.distance_modulus_th(self.zcmb,self.zhl)))
+        else:
+            self.model.set(z=self.zhl)
+            Xgr_init = 10**(-0.4*(ct.distance_modulus_th(self.zcmb,self.zhl)))
         print 'initialisation'            
 
         self.model.set(q1=0.0)
         self.model.set(q2=0.0)
-        self.model.set(q3=0.0)     
-        self.model.set(Xgr=1.0e-15)
-        
+        self.model.set(q3=0.0)
+        self.model.set(A=0.1)
+        self.model.set(Xgr=Xgr_init)
         if self.t0_fix:
             self.model.set(t0=self.daymax)
-            res, fitted_model = sncosmo.fit_lc(data, 
-                                               self.model, 
-                                               ['A','Xgr'], 
-                                               modelcov=False)
             res, fitted_model = sncosmo.fit_lc(data, 
                                                self.model, 
                                                ['q1', 
@@ -127,18 +162,28 @@ class LC_Fitter(object):
                                                ['t0','A','Xgr'], 
                                                modelcov=False)
     
-            print res.parameters[1] 
-            print 'first iteration'
             chi2 = res.chisq
             chi2p = chi2*2
             m=0
+            recovery = True
+            print res.parameters[1] 
+            t0_init = res.parameters[1] 
+            Xgr_init = res.parameters[2]
+            self.model.set(A=res.parameters[6])
+            self.model.set(Xgr=res.parameters[2])
+            self.model.set(t0=res.parameters[1]) 
+            res, fitted_model = sncosmo.fit_lc(data, 
+                                               self.model, 
+                                               ['q1','q2','q3'], 
+                                               modelcov=False)
+            
+            print 'first iteration'
             while chi2 < chi2p and m < 10:
     
                 print m
                 if m > 0:
                     resp = res
                     fitted_modelp = fitted_model
-                m += 1
                 t_peak = fitted_model.parameters[1]
                 t1 = t_peak + cst.t_min_sug*(1 + self.model.get('z'))
                 t2 = t_peak + cst.t_max_sug*(1 + self.model.get('z'))
@@ -172,15 +217,60 @@ class LC_Fitter(object):
                                                     'Xgr'], 
                                                    modelcov  = self.modelcov,
                                                    bounds = {'t0' : [res.parameters[1]-30,res.parameters[1]+30]})
-                
+            
                 chi2p = chi2
                 chi2 = res.chisq
                 print chi2p, chi2
+                if m == 0 and chi2 > chi2p :
+                    if recovery:
+                        print 'First iteration fail try to recovery :'
+                        chi2 = self.recovery_sugar(data, t0_init, Xgr_init, res, chi2p)
+                        recovery = False 
+                    else:
+                        raise ValueError('Error initial value better than first iteration')
+                else:
+                    m += 1
+            
             #final results
             res = resp
             fitted_model = fitted_modelp
-        return res, fitted_model    
-
+        return res, fitted_model 
+    
+    def recovery_sugar(self, data, t0_init, Xgr_init, res, chi2p):
+        n=0
+        t0_iter=t0_init-5
+        while res.chisq > chi2p and n < 10:
+            print n
+            n+=1
+    
+            self.model.set(Xgr=Xgr_init)
+            self.model.set(q1=0.0)
+            self.model.set(q2=0.0)
+            self.model.set(q3=0.0)
+            self.model.set(A=0.2)
+            self.model.set(t0=t0_iter)   
+            try:
+                res, fitted_model = sncosmo.fit_lc(data, 
+                                           self.model, 
+                                           ['t0', 
+                                            'q1', 
+                                            'q2', 
+                                            'q3', 
+                                            'A', 
+                                            'Xgr'], 
+                                           modelcov  = self.modelcov,
+                                           bounds = {'t0' : [res.parameters[1]-5,res.parameters[1]+5]}) 
+            except:
+                'Fit fail in recovery iteration %f! Next iteration'%n
+            t0_iter += 1
+            print res.chisq
+        if n == 10:
+            raise ValueError('recovery fail')
+        else:
+            print 'successful recovery retry to fit'
+        
+        return res.chisq
+            
     def fit_lc_salt2(self, data, zhl=None,  mwebv=None ):
         """
         """
@@ -193,7 +283,8 @@ class LC_Fitter(object):
                               effect_frames=['obs']) 
             self.mwebv = mwebv
             self.zhl = zhl
-            
+
+                
         self.model.set(mwebv=self.mwebv)
         self.model.set(z=self.zhl)
         print 'initialisation'            
@@ -272,12 +363,13 @@ class LC_Fitter(object):
                               effects=[dust], 
                               effect_names=['mw'], 
                               effect_frames=['obs'])
-        for sn_name in self.data.keys():
+        for sn_name in self.data:
             self.sn_data(sn_name)
             print sn_name
             if self.model_name == 'sugar':
                 try:
                     res_sn, fitted_model_sn = self.fit_lc_sugar(self.table_sn)
+#                    sncosmo.plot_lc(self.table_sn, fitted_model_sn)
                 except :
                     print 'fit fail for '+sn_name
                     res_sn, fitted_model_sn = 'fit fail', np.nan
@@ -285,14 +377,18 @@ class LC_Fitter(object):
             elif self.model_name == 'salt2':
                 try :
                     res_sn, fitted_model_sn = self.fit_lc_salt2(self.table_sn)
+                    
                 except :
                     print 'fit fail for '+sn_name
                     res_sn, fitted_model_sn = 'fit fail', np.nan
                     self.fit_fail.append(sn_name)
+                    
             else :
                 raise ValueError('Error model_name have to be salt2 or sugar')
             self.fitted_model.append(fitted_model_sn)
             self.dic_res['data'][sn_name] = {}
+#            self.dic_res['data'][sn_name]['fitted_model'] = fitted_model_sn
+#            self.dic_res['data'][sn_name]['data_table'] = self.table_sn
             self.dic_res['data'][sn_name]['res'] = res_sn
             self.dic_res['data'][sn_name]['zhl'] = self.zhl
             self.dic_res['data'][sn_name]['zerr'] = self.zerr
@@ -307,7 +403,6 @@ class LC_Fitter(object):
             raise ValueError('fitted sample needed to write result')
         else:
             File = open(output_path+'sugar_analysis_data/resfitlc_'+self.sample+'_'+self.model_name+'.pkl','w')
-            
             pkl.dump(self.dic_res, File)
         
 
