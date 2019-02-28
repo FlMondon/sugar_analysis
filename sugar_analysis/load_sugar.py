eaac6843465cf0b4f3d58f9b01706b1927e89111
FlMondon <florian.mondon@clermont.in2p3.fr>
2019-02-28 20:24:43 +0100

    add load sugar

---
+++ sugar_analysis/load_sugar.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 21 17:59:43 2019
+
+@author: florian
+"""
+import sncosmo
+import numpy as np
+import os
+from astropy.extern import six
+from sncosmo.models import _SOURCES
+
+
+class SUGARSource(sncosmo.Source):
+    _param_names = ['Xgr', 'q1', 'q2', 'q3', 'A']
+    param_names_latex = ['X_r', 'q_1', 'q_2', 'q_3', 'A']
+    
+
+
+    def __init__(self, modeldir='../../sugar_model/', m0file='sugar_template_0.dat',
+                 m1file='sugar_template_1.dat',
+            		m2file='sugar_template_2.dat',
+            		m3file='sugar_template_3.dat',
+                 m4file='sugar_template_4.dat',
+                 name=None, version=None):
+
+        
+        self.name = name
+        self.version = version
+        self._model = {}
+        self._parameters = np.array([1.0e10-15, 1., 1., 1., 0.])
+        self.M_keys = ['M0', 'M1', 'M2', 'M3', 'M4']    
+        names_or_objs = {'M0': m0file, 'M1': m1file, 'M2': m2file, 'M3': m3file, 'M4': m4file}
+
+        # Make filenames into full paths.
+        if modeldir is not None:
+            for k in names_or_objs:
+                v = names_or_objs[k]
+                if (v is not None and isinstance(v, six.string_types)):
+                    names_or_objs[k] = os.path.join(modeldir, v)
+                    
+        for i, key in enumerate(self.M_keys):
+            phase, wave, values = sncosmo.read_griddata_ascii(names_or_objs[key])
+            self._model[key] = sncosmo.salt2utils.BicubicInterpolator(phase, wave, values) 
+            if key == 'M0':
+                # The "native" phases and wavelengths of the model are those
+                self._phase = np.array([ -12.,  -9.,  -6.,  -3.,   0.,   3.,   6.,   9.,  12.,  15.,  18.,
+                                                21.,  24.,  27.,  30.,  33.,  36.,  39.,  42.,  45.,  48.])
+                self._wave = wave
+
+    def _flux(self, phase, wave):
+        M_sug = 48.59
+        for i, key in enumerate(self.M_keys):
+            if i >= 1.: 
+                M_sug += self._model[key](phase, wave) * self._parameters[i]
+            else: 
+                M_sug += self._model[key](phase, wave)
+        wave2_factor = (wave ** 2 / 299792458. * 1.e-10)
+        return (self._parameters[0] * 10. ** (-0.4 * M_sug) /  wave2_factor)
+
+
+    def bandflux_rcov(self, band, phase):
+        return np.zeros(phase.shape, dtype=np.float64)
+    
+
+# Sugar model
+def load_sugarmodel(relpath, name=None, version=None):
+    return SUGARSource(modeldir=relpath, name=name, version=version)
+
+def register_SUGAR(sugar_model='../../sugar_model/'):
+    website = 'http://no'
+    PF16ref = ('PF16', 'PF et al. 2016 '
+              '<http://arxiv.org/>')
+    for topdir, ver, ref in [('SUGAR_model', '1.0', PF16ref)]:
+        meta = {'type': 'SN Ia', 'subclass': '`~sncosmo.SUGARSource`',
+                'url': website, 'reference': ref}
+        _SOURCES.register_loader('sugar', load_sugarmodel, args=([sugar_model]), version=ver, meta=meta, force=True)
